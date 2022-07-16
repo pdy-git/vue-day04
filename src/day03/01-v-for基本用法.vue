70f07bba421645e01a0556b0522ce9b605845f50
pdy-gitee <pdy_1214@163.com>
2022-07-16 21:26:57 +0800

    1

---
+++ src/day03/01-v-for基本用法.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <!-- 语法1:
+      v-for="(值变量名, 索引变量名) in 目标结构"
+      口诀: 想要谁循环就放到谁身上
+
+      需求：将数组 ['zhangsan', 'lisi', 'wangwu', 'dlrb'] 
+      属性key: 唯一值 类型：string/number 作用是提高性能
+     -->
+    <p>学生姓名</p>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+
+    <!-- 
+    需求2: 将学生的具体信息渲染到页面上
+    [ { id: 1001, name: "孙悟空", sex: "男", hobby: "吃桃子", },
+      { id: 1002, name: "猪八戒", sex: "男", hobby: "背媳妇", }, 
+    ], -->
+    <p>学生详情</p>
+    <ul>
+      <li v-for="item in list1" :key="item.id">
+        {{ item.name }}--{{ item.sex }}--{{ item.hobby }}
+      </li>
+    </ul>
+
+    <!-- 
+    需求3: 将老师信息展示到页面上
+    {  name: "小黑", age: 18, class: "1期", }
+     -->
+    <p>将老师信息展示到页面上</p>
+    <ul>
+      <li v-for="(item, key) in obj" :key="item">{{ item }}--{{ key }}</li>
+    </ul>
+
+    <!-- 需求4: 遍历整数 10 -->
+    <p>遍历整数 10</p>
+    <ul>
+      <li v-for="item in sum" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Demo01VFor",
+  data () {
+    return {
+      list: ["zhangsan", "lisi", "wangwu", "wangwu", "dlrb"],
+      list1: [
+        { id: 1001, name: "孙悟空", sex: "男", hobby: "吃桃子" },
+        { id: 1002, name: "猪八戒", sex: "男", hobby: "背媳妇" },
+      ],
+      obj: { name: "小黑", age: 18, class: "1期" },
+      num: 10,
+    };
+  },
+};
+</script>
---
+++ src/day03/02-数组的方法变更触发vue视图更新.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(val, index) in arr" :key="index">
+        {{ val }}
+      </li>
+    </ul>
+    <button @click="btn">数组翻转</button>
+    <button @click="btn1">截取前3个</button>
+    <button @click="btn2">更新第一个元素值</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Demo02Test",
+  data () {
+    return {
+      arr: [5, 3, 9, 2, 1],
+    };
+  },
+  methods: {
+    btn () {
+      this.arr.reverse();
+    },
+    btn1 () {
+      this.arr.slice(0, 3);
+      // console.log(this.arr.slice(0, 3));
+    },
+    btn2 () {
+      this.arr[0] = 1000;
+      // console.log(this.arr);
+      this.$set(this.arr, 1, 2000);
+    },
+  },
+};
+</script>
